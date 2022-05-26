203d612545e180bdd2b1e4f8eb4f63154ef1cee3
Muhammad Hunain <92049246+hunain3199@users.noreply.github.com>
2022-05-26 19:37:55 +0500

    Delete fake_dating_profiles directory

--- fake_dating_profiles/columns.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-df = pd.read_csv('fake_data.csv')
-df['Body Type'] = 'Large'
-df.to_csv('fake_data.csv',index=False)
-
-# df_2 = pd.read_csv("fake_data.csv", header=None)
-# df_2.to_csv("fake_data.csv", header=["Name", "Height","Age","Birthdate"])
